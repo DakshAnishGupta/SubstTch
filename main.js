c22dbf6aa6246cb288551baa48540b7db0269726
DakshAnishGupta <103361449+DakshAnishGupta@users.noreply.github.com>
2022-08-23 18:05:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+img="";
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center()
+}
+
+function preload()
+{
+    img = loadImage("dog_cat.jpg");
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 450);
+    fill("#0000FF");
+    text("Dog", 45, 75);
+    nofill();
+    stroke("#00FFFF");
+    rect(30, 60, 450, 640);
+}
