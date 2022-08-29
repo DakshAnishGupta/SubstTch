3e69a71ba9ee350de3956a8de6da1e7872e41db7
DakshAnishGupta <103361449+DakshAnishGupta@users.noreply.github.com>
2022-08-29 17:36:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
 img="";
+objects=[];
+status="";
 
 function setup()
 {
     canvas = createCanvas(640, 420);
-    canvas.center()
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="status:detecting objects";
+}
+
+function modelLoaded()
+{
+console.log("model Loaded!");
+status = true;
+objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+if(error)
+{
+    console.log(error);
+}
+console.log(results);
+
 }
 
 function preload()
@@ -19,4 +40,7 @@ function draw()
     noFill();
     stroke("#FF0000");
     rect(30, 60, 450, 640);
+
+    fill("Blue");
+text("cat",200, 200);
 }
